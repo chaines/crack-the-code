c39e7d22e817c6691912740e306e3ddfb92f7470
Cody Haines <chaines51@gmail.com>
2017-03-28 23:00:32 +0000

    Added question to 1.1

--- Chapter 1/1.1.js
+++ Chapter 1/1.1.js
@@ -1,3 +1,8 @@
+/**
+ * Is Unique: Implement an algorithm to determine if a string has all unique 
+ * characters. What if you cannot use additional data structures?
+ **/
+
 function isUnique(string) {
     var inString = [];
     for(var i = 0; i < string.length; i++) {
