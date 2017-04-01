a67c88b1fbc3472f4cf18a23408eeea8fda7df04
Cody Haines <chaines51@gmail.com>
2017-04-01 19:18:39 +0000

    Added tests for 1.6

--- Chapter 1/Tests/1.6.test.js
+++ Chapter 1/Tests/1.6.test.js
@@ -6,5 +6,13 @@ vows.describe("Simple String Compression").addBatch({
     "Example string: 'aabcccccaaa'": {
         topic: function() { return compressString('aabcccccaaa') },
         "expect 'a2b1c5a3'": function(topic) { assert.equal(topic, 'a2b1c5a3') }
+    },
+    "String that shouldn't be compresed: 'abcdef'": {
+        topic: function() { return compressString('abcdef') },
+        "expect uncompressed 'abcdef'": function(topic) { assert.equal(topic, 'abcdef') }
+    },
+    "String with the same character in multiple spots: 'aaaabbbbaaaa'": {
+        topic: function() { return compressString('aaaabbbbaaaa') },
+        "expect: 'a4b4a4'": function(topic) { assert.equal(topic, 'a4b4a4') }
     }
 }).export(module)
